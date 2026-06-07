13031a489401a66fe1831a07dab6a1d2f0a8fbab
xzwpuri <junmin7puuri2009@naver.com>
2026-06-07 16:41:17 +0900

    플레이어 점프 버그 수정
    - 땅에서 점프와 공중 더블점프 조건 분리
    - 땅에서는 isGrounded 체크, 공중에서는 jumpCount 체크
    - 점프 직후 Ground 감지로 jumpCount 리셋되던 문제 해결

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,25 @@
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] float moveSpeed = 8f;
-
+    [SerializeField] float moveSpeed = 6f;
     [Header("Jump")]
-    [SerializeField] float jumpForce = 14f;
+    [SerializeField] float jumpForce = 15f;
     [SerializeField] int maxJumpCount = 2;
-
     [Header("Gravity")]
-    [SerializeField] float fallGravityScale = 4f;   // 낙하 시 중력
-    [SerializeField] float riseGravityScale = 2f;   // 상승 시 중력
-
+    [SerializeField] float fallGravityScale = 5f;
+    [SerializeField] float riseGravityScale = 5f;
     [Header("Ground Check")]
     [SerializeField] Transform groundCheck;
     [SerializeField] float groundCheckRadius = 0.1f;
     [SerializeField] LayerMask groundLayer;
-
     Rigidbody2D rb;
     int jumpCount;
     bool isGrounded;
-
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
     void Update()
     {
         CheckGround();
@@ -34,32 +27,37 @@ public class PlayerController : MonoBehaviour
         Jump();
         UpdateGravity();
     }
-
     void CheckGround()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        if (isGrounded)
+        bool wasGrounded = isGrounded;
+        Collider2D hit = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        isGrounded = hit != null;
+        if (!wasGrounded && isGrounded)
             jumpCount = 0;
     }
-
     void Move()
     {
         float input = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(input * moveSpeed, rb.linearVelocity.y);
-
         if (input > 0) transform.localScale = new Vector3(1, 1, 1);
         else if (input < 0) transform.localScale = new Vector3(-1, 1, 1);
     }
-
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumpCount)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            jumpCount++;
+            if (isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                jumpCount = 1;
+            }
+            else if (jumpCount < maxJumpCount)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                jumpCount++;
+            }
         }
     }
-
     void UpdateGravity()
     {
         if (rb.linearVelocity.y < 0)
@@ -67,7 +65,6 @@ public class PlayerController : MonoBehaviour
         else
             rb.gravityScale = riseGravityScale;
     }
-
     void OnDrawGizmosSelected()
     {
         if (groundCheck != null)
